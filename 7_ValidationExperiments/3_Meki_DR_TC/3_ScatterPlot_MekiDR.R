99e4a76dcb635f5415d93300e498079f2eaf01d4
Zeba Sultana <zeba.sultana@gmail.com>
2023-03-20 01:58:59 +0100

    Updated Scrpit 3_Scatterplot to include tests for significance of results

--- 7_ValidationExperiments/3_Meki_DR_TC/3_ScatterPlot_MekiDR.R
+++ 7_ValidationExperiments/3_Meki_DR_TC/3_ScatterPlot_MekiDR.R
@@ -68,7 +68,46 @@ g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_no1000_Mek, aes(x=x_axis_factor,y=S
 
 gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
 grid.arrange(gt)
-ggsave("Fig7Gi_PDDR_XX_XO_compared_pMek.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
+ggsave("Fig7Gi_PDDR_XX_XO_compared_pMek.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER")
+
+
+# Test for significance of results : Fig7G_Mek
+
+Mek_XX_0= PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+  filter(Cell_line=="XX" & Treatment_Fctr ==0) 
+
+Mek_XO_0 = PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 0) 
+
+Mek_XO_4 = PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 4)
+
+Mek_XO_12 = PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 12) 
+
+
+Fig7G_Mek_compared_groups=list("Mek_XO_0"=Mek_XO_0,"Mek_XO_4"=Mek_XO_4,"Mek_XO_12"=Mek_XO_12)
+Fig7G_Mek_compared_groups_ttest=list()
+
+for(i in 1:length(Fig7G_Mek_compared_groups)) {
+  
+  Fig7G_Mek_compared_groups_ttest$compared_group[i] = names(Fig7G_Mek_compared_groups[i])
+  
+  unpaired_Ttest = t.test(Mek_XX_0$Signal,Fig7G_Mek_compared_groups[[i]]$Signal, paired = FALSE)
+  
+  Fig7G_Mek_compared_groups_ttest$XX_untreated_mean[i] = unpaired_Ttest$estimate[1]
+  Fig7G_Mek_compared_groups_ttest$XO_treated_mean[i] = unpaired_Ttest$estimate[2]
+  Fig7G_Mek_compared_groups_ttest$p_value[i] = unpaired_Ttest$p.value
+  
+}
+
+Fig7G_Mek_compared_groups_ttest <- data.frame((sapply(Fig7G_Mek_compared_groups_ttest,c)))
+Fig7G_Mek_compared_groups_ttest$p_value = as.numeric(as.character(Fig7G_Mek_compared_groups_ttest$p_value))
+Fig7G_Mek_compared_groups_ttest <- Fig7G_Mek_compared_groups_ttest %>% 
+  mutate(sig = ifelse(p_value<0.05,"*",""))
+
+WriteXLS::WriteXLS(Fig7G_Mek_compared_groups_ttest, "./OUTPUT_PAPER/Fig7Gi_Mek_Ttest.xls")
+
 
 
 ####### FC compared : Fig7H #########
@@ -94,6 +133,36 @@ g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek, aes(x=x_axis_factor,
 gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
 grid.arrange(gt)
 ggsave("Fig7H_PDDR_XX_XO_compared_pMek_FC_NEW.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
+
+# Test for significance of results : Fig7H
+
+Fig7H_XX = PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek 
+
+Fig7H_ttest = list()
+the_cell_lines = c("XX","XO")
+for(i in 1:length(the_cell_lines)){
+  
+  data = Fig7H_XX %>% 
+    filter(Cell_line == the_cell_lines[i])
+  
+  treated_signal = data %>% filter(Treatment == 1000) %>% select(Signal)
+  base_signal = data %>% filter(Treatment != 1000) %>% select(Signal)
+  
+  test_result = t.test(base_signal,treated_signal, paired = F)
+  
+  Fig7H_ttest$cell_line[i] = the_cell_lines[i]
+  Fig7H_ttest$base_mean[i] = test_result$estimate[1]
+  Fig7H_ttest$treated_mean[i] = test_result$estimate[2]
+  Fig7H_ttest$p_value[i] = test_result$p.value
+  
+}
+
+Fig7H_ttest <- data.frame((sapply(Fig7H_ttest,c)))
+Fig7H_ttest$p_value = as.numeric(as.character(Fig7H_ttest$p_value))
+Fig7H_ttest <- Fig7H_ttest %>% 
+  mutate(sig = ifelse(p_value<0.05,"*",""))
+
+WriteXLS::WriteXLS(Fig7H_ttest, "./OUTPUT_PAPER/Fig7H_Ttest.xls")
 
 
 
@@ -137,5 +206,44 @@ g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_Raf, aes(x=x_axis_factor,y=Signal))
 gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
 grid.arrange(gt)
 ggsave("Fig7Gii_PDDR_XX_XO_compared_pRaf.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
+
+# Test for significance of results : Fig7G_Raf
+
+Raf_XX_0= PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
+  filter(Cell_line=="XX" & Treatment_Fctr ==0) 
+
+Raf_XO_0 = PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 0) 
+
+Raf_XO_4 = PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 4)
+
+Raf_XO_12 = PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 12) 
+
+
+Fig7G_Raf_compared_groups=list("Raf_XO_0"=Raf_XO_0,"Raf_XO_4"=Raf_XO_4,"Raf_XO_12"=Raf_XO_12)
+Fig7G_Raf_compared_groups_ttest=list()
+
+for(i in 1:length(Fig7G_Raf_compared_groups)) {
+  
+  Fig7G_Raf_compared_groups_ttest$compared_group[i] = names(Fig7G_Raf_compared_groups[i])
+  
+  unpaired_Ttest = t.test(Raf_XX_0$Signal,Fig7G_Raf_compared_groups[[i]]$Signal, paired = FALSE)
+  
+  Fig7G_Raf_compared_groups_ttest$XX_untreated_mean[i] = unpaired_Ttest$estimate[1]
+  Fig7G_Raf_compared_groups_ttest$XO_treated_mean[i] = unpaired_Ttest$estimate[2]
+  Fig7G_Raf_compared_groups_ttest$p_value[i] = unpaired_Ttest$p.value
+  
+}
+
+Fig7G_Raf_compared_groups_ttest <- data.frame((sapply(Fig7G_Raf_compared_groups_ttest,c)))
+Fig7G_Raf_compared_groups_ttest$p_value = as.numeric(as.character(Fig7G_Raf_compared_groups_ttest$p_value))
+Fig7G_Raf_compared_groups_ttest <- Fig7G_Raf_compared_groups_ttest %>% 
+  mutate(sig = ifelse(p_value<0.05,"*",""))
+
+WriteXLS::WriteXLS(Fig7G_Raf_compared_groups_ttest, "./OUTPUT_PAPER/Fig7Gii_Raf_Ttest.xls")
+
+
 
 print("Script 3 executed : Scatter plots saved")
