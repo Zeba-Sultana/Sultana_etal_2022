9af02d94782aaa87b6c356e677fcd68f10a56cb8
Zeba Sultana <zeba.sultana@gmail.com>
2023-04-02 01:24:55 +0200

    Updated Script3 for saving Fig 7G and H data as xls

--- 7_ValidationExperiments/3_Meki_DR_TC/3_ScatterPlot_MekiDR.R
+++ 7_ValidationExperiments/3_Meki_DR_TC/3_ScatterPlot_MekiDR.R
@@ -12,54 +12,46 @@ if(!(file.exists("./OUTPUT_PAPER"))){
 source("../ValidationPlot_Functions/U_Functions_ValidationExp_Analysis.R")
 source("../ValidationPlot_Functions/U_Functions_ValidationExp_Plotting.R")
 
-PDDR_All_MekTPSplot_FCoXX <- read.csv("OUTPUT_MekiDR/PDDR_All_MekTPSplot_FCoXX.csv")
+PDDR_All_FCoXX <- read.csv("OUTPUT_MekiDR/PDDR_All_MekTPSplot_FCoXX.csv")
 
-PDDR_All_MekTPSplot_FCoXX$Analyte = factor(PDDR_All_MekTPSplot_FCoXX$Analyte, levels =c("FCoXX_M2_Mek", "FCoXX_M2_cRaf")) # To get the correct sequence in Facet_wrap
+PDDR_All_FCoXX$Analyte = factor(PDDR_All_FCoXX$Analyte, levels =c("FCoXX_M2_Mek", "FCoXX_M2_cRaf")) # This is to get the correct sequence in the Facet_wrap
 Analyte_labels <- c("FCoXX_M2_Mek" = "pMEK", "FCoXX_M2_cRaf" = "pRAF1")
-PDDR_All_MekTPSplot_FCoXX$Cell_line = factor(PDDR_All_MekTPSplot_FCoXX$Cell_line, levels =c("XX", "XO")) # To get the correct sequence in Facet_wrap
+PDDR_All_FCoXX$Cell_line = factor(PDDR_All_FCoXX$Cell_line, levels =c("XX", "XO")) # This is to get the correct sequence in the Facet_wrap
 
 
-PDDR_All_MekTPSplot_FCoXX_subset_Mek <- PDDR_All_MekTPSplot_FCoXX %>% 
-  filter((Cell_line == "XX" & (Treatment_Fctr == 0|Treatment_Fctr == 1000))|(Cell_line == "XO" & (Treatment_Fctr == 0|Treatment_Fctr == 4|Treatment_Fctr == 12|Treatment_Fctr == 1000))) %>%
-  filter(grepl("Mek", Analyte))
+PDDR_All_FCoXX_SUBSET = PDDR_All_FCoXX %>% 
+  filter(Treatment %in% c(0,4,12,1000))
 
-PDDR_All_MekTPSplot_ComparativeFC_subset_Mek <- PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
-  mutate(Baseline_XX = mean(Signal[Treatment == 0 & Cell_line == "XX"])) %>% 
-  mutate(Baseline_XO = mean(Signal[Treatment == 4 & Cell_line == "XO"])) %>% 
-  mutate(Comparative_FC = ifelse(Cell_line == "XX", Signal/Baseline_XX, Signal/Baseline_XO) )
-
-  
-PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$Treatment_Fctr = factor(PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$Treatment_Fctr,levels = c(0,4,12,1000))
-PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$x_axis_factor <- paste0(PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$Cell_line,":",PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$Treatment_Fctr)
-PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$x_axis_factor <- factor(PDDR_All_MekTPSplot_ComparativeFC_subset_Mek$x_axis_factor, 
-                                                             levels = c("XX:0",
-                                                                        "XX:1000",
-                                                                        "XO:0",
-                                                                        "XO:4",
-                                                                        "XO:12",
-                                                                        "XO:1000"),
-                                                             labels = c("XX:0",
-                                                                        "XX:1000",
-                                                                        "0",
-                                                                        "4",
-                                                                        "12",
-                                                                        "1000"))
+PDDR_All_FCoXX_SUBSET$Treatment_Fctr = factor(PDDR_All_FCoXX_SUBSET$Treatment_Fctr,levels = c(0,4,12,1000))
+PDDR_All_FCoXX_SUBSET$x_axis_factor <- paste0(PDDR_All_FCoXX_SUBSET$Cell_line,":",PDDR_All_FCoXX_SUBSET$Treatment_Fctr)
+PDDR_All_FCoXX_SUBSET$x_axis_factor <- factor(PDDR_All_FCoXX_SUBSET$x_axis_factor, 
+                                              levels = c("XX:0",
+                                                         "XX:1000",
+                                                         "XO:0",
+                                                         "XO:4",
+                                                         "XO:12",
+                                                         "XO:1000"),
+                                              labels = c("XX:0",
+                                                         "XX:1000",
+                                                         "0",
+                                                         "4",
+                                                         "12",
+                                                         "1000"))
 
 
+########### Fig 7G : Mek ###########
 
-#################### Fig7Gi : Mek
+Fig7G_pMek = PDDR_All_FCoXX_SUBSET %>% 
+  filter(Analyte == "FCoXX_M2_Mek") %>% 
+  filter(Treatment != 1000) %>% 
+  filter(!(Cell_line == "XX" & (Treatment == 4|Treatment == 12)))
 
-PDDR_All_MekTPSplot_FCoXX_subset_no1000_Mek <- PDDR_All_MekTPSplot_ComparativeFC_subset_Mek %>% 
-  filter(Treatment != 1000)
-
-g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_no1000_Mek, aes(x=x_axis_factor,y=Signal)) +
-  #geom_point(alpha = 0.8,aes(x=x_axis_factor,y=Signal,color=Cell_line))+
+g <- ggplot(Fig7G_pMek, aes(x=x_axis_factor,y=Signal)) +
   geom_point(aes(x=x_axis_factor,y=Signal,color=Cell_line))+
   scale_fill_manual(values = MyCellLineColours) +
   scale_color_manual(values = MyCellLineColours) +
-  #facet_grid(~Cell_line, space = "free",scales = "free", switch = "x")+
   stat_summary(fun = mean,geom = "crossbar", size = 0.1, position = position_dodge(width = 1))+
-  ylim(0,max(PDDR_All_MekTPSplot_FCoXX_subset_no1000_Mek$Signal)+0.5)+
+  ylim(0,max(Fig7G_pMek$Signal)+0.5)+
   labs(x = "\n MEKi(nM)", 
        y = " Rel. phosp. (norm.) ",
        title = "pMEK",
@@ -68,21 +60,20 @@ g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_no1000_Mek, aes(x=x_axis_factor,y=S
 
 gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
 grid.arrange(gt)
-ggsave("Fig7Gi_PDDR_XX_XO_compared_pMek.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER")
-
+ggsave("Fig7G_pMek.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
 
 # Test for significance of results : Fig7G_Mek
 
-Mek_XX_0= PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+Mek_XX_0= Fig7G_pMek %>% 
   filter(Cell_line=="XX" & Treatment_Fctr ==0) 
 
-Mek_XO_0 = PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+Mek_XO_0 = Fig7G_pMek %>% 
   filter(Cell_line=="XO" & Treatment_Fctr == 0) 
 
-Mek_XO_4 = PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+Mek_XO_4 = Fig7G_pMek %>% 
   filter(Cell_line=="XO" & Treatment_Fctr == 4)
 
-Mek_XO_12 = PDDR_All_MekTPSplot_FCoXX_subset_Mek %>% 
+Mek_XO_12 = Fig7G_pMek %>% 
   filter(Cell_line=="XO" & Treatment_Fctr == 12) 
 
 
@@ -104,27 +95,116 @@ for(i in 1:length(Fig7G_Mek_compared_groups)) {
 Fig7G_Mek_compared_groups_ttest <- data.frame((sapply(Fig7G_Mek_compared_groups_ttest,c)))
 Fig7G_Mek_compared_groups_ttest$p_value = as.numeric(as.character(Fig7G_Mek_compared_groups_ttest$p_value))
 Fig7G_Mek_compared_groups_ttest <- Fig7G_Mek_compared_groups_ttest %>% 
-  mutate(sig = ifelse(p_value<0.05,"*",""))
+  mutate(sig = ifelse(p_value<0.05,"*","")) %>% 
+  separate(compared_group, into = c("Analyte1","Cell_line","Treatment"), sep = "_", fill = "right") %>% 
+  select(-c(Analyte1)) %>% 
+  mutate(Treatment=as.numeric(as.character(Treatment)))
 
-WriteXLS::WriteXLS(Fig7G_Mek_compared_groups_ttest, "./OUTPUT_PAPER/Fig7Gi_Mek_Ttest.xls")
+Fig7G_Mek_Replicates = Fig7G_pMek %>% 
+  select(Cell_line,Replicate,Treatment,log2Treatment,Analyte,Signal) %>% 
+  mutate(Treatment=as.numeric(as.character(Treatment))) %>%
+  pivot_wider(names_from = Replicate,values_from=Signal) %>% 
+  rowwise() %>% 
+  mutate(Mean=mean(c(R1,R2,R3), na.rm = TRUE))
 
+Fig7G_Mek_data = left_join(Fig7G_Mek_Replicates,Fig7G_Mek_compared_groups_ttest, by=c("Treatment","Cell_line")) %>% 
+  arrange(desc(Cell_line)) %>% 
+  mutate(Analyte= gsub("FCoXX_M2_","",Analyte))
 
+########### Fig 7G : cRaf ###########
 
-####### FC compared : Fig7H #########
+Fig7G_pcRaf = PDDR_All_FCoXX_SUBSET %>% 
+  filter(Analyte == "FCoXX_M2_cRaf") %>% 
+  filter(Treatment != 1000) %>% 
+  filter(!(Cell_line == "XX" & (Treatment == 4|Treatment == 12)))
 
-PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek <- PDDR_All_MekTPSplot_ComparativeFC_subset_Mek %>% 
-  filter(Treatment != 12) %>% 
-  filter(!(Cell_line =="XO" & Treatment == 0))
-
-g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek, aes(x=x_axis_factor,y=Signal)) +
-  #geom_point(alpha = 0.8,aes(x=x_axis_factor,y=Signal,color=Cell_line))+
+g <- ggplot(Fig7G_pcRaf, aes(x=x_axis_factor,y=Signal)) +
   geom_point(aes(x=x_axis_factor,y=Signal,color=Cell_line))+
   scale_fill_manual(values = MyCellLineColours) +
   scale_color_manual(values = MyCellLineColours) +
-  #facet_grid(~Cell_line, space = "free",scales = "free", switch = "x")+
   stat_summary(fun = mean,geom = "crossbar", size = 0.1, position = position_dodge(width = 1))+
-  ylim(0,max(PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek$Signal)+0.5)+
-  labs(x = "\n MEKi(nM)", #\u03bc is the unicode charachter fro greek
+  ylim(0.5,max(Fig7G_pcRaf$Signal)+0.5)+
+  labs(x = "\n cRafi(nM)", 
+       y = " Rel. phosp. (norm.) ",
+       title = "pcRaf",
+       color = "Cell line" )+
+  MyPaperTheme
+
+gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
+grid.arrange(gt)
+ggsave("Fig7G_pcRaf.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
+
+# Test for significance of results : Fig7G_cRaf
+
+cRaf_XX_0= Fig7G_pcRaf %>% 
+  filter(Cell_line=="XX" & Treatment_Fctr ==0) 
+
+cRaf_XO_0 = Fig7G_pcRaf %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 0) 
+
+cRaf_XO_4 = Fig7G_pcRaf %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 4)
+
+cRaf_XO_12 = Fig7G_pcRaf %>% 
+  filter(Cell_line=="XO" & Treatment_Fctr == 12) 
+
+
+Fig7G_cRaf_compared_groups=list("cRaf_XO_0"=cRaf_XO_0,"cRaf_XO_4"=cRaf_XO_4,"cRaf_XO_12"=cRaf_XO_12)
+Fig7G_cRaf_compared_groups_ttest=list()
+
+for(i in 1:length(Fig7G_cRaf_compared_groups)) {
+  
+  Fig7G_cRaf_compared_groups_ttest$compared_group[i] = names(Fig7G_cRaf_compared_groups[i])
+  
+  unpaired_Ttest = t.test(cRaf_XX_0$Signal,Fig7G_cRaf_compared_groups[[i]]$Signal, paired = FALSE)
+  
+  Fig7G_cRaf_compared_groups_ttest$XX_untreated_mean[i] = unpaired_Ttest$estimate[1]
+  Fig7G_cRaf_compared_groups_ttest$XO_treated_mean[i] = unpaired_Ttest$estimate[2]
+  Fig7G_cRaf_compared_groups_ttest$p_value[i] = unpaired_Ttest$p.value
+  
+}
+
+Fig7G_cRaf_compared_groups_ttest <- data.frame((sapply(Fig7G_cRaf_compared_groups_ttest,c)))
+Fig7G_cRaf_compared_groups_ttest$p_value = as.numeric(as.character(Fig7G_cRaf_compared_groups_ttest$p_value))
+Fig7G_cRaf_compared_groups_ttest <- Fig7G_cRaf_compared_groups_ttest %>% 
+  mutate(sig = ifelse(p_value<0.05,"*","")) %>% 
+  separate(compared_group, into = c("Analyte1","Cell_line","Treatment"), sep = "_", fill = "right") %>% 
+  select(-c(Analyte1)) %>% 
+  mutate(Treatment=as.numeric(as.character(Treatment)))
+
+Fig7G_cRaf_Replicates = Fig7G_pcRaf %>% 
+  select(Cell_line,Replicate,Treatment,log2Treatment,Analyte,Signal) %>% 
+  mutate(Treatment=as.numeric(as.character(Treatment))) %>%
+  pivot_wider(names_from = Replicate,values_from=Signal) %>% 
+  rowwise() %>% 
+  mutate(Mean=mean(c(R1,R2,R3), na.rm = TRUE))
+
+Fig7G_cRaf_data = left_join(Fig7G_cRaf_Replicates,Fig7G_cRaf_compared_groups_ttest, by=c("Treatment","Cell_line")) %>% 
+  arrange(desc(Cell_line)) %>% 
+  mutate(Analyte= gsub("FCoXX_M2_","",Analyte))
+
+
+########### Fig 7G : writing out the data as xls ###########
+Fig7G_Mek_cRaf_data = dplyr::bind_rows(Fig7G_Mek_data,Fig7G_cRaf_data)
+WriteXLS::WriteXLS(Fig7G_Mek_cRaf_data, "./OUTPUT_PAPER/Fig7G_Mek_cRaf_data.xls")
+
+
+######################## FIG 7H ###############
+
+Fig7H_pMek = PDDR_All_FCoXX_SUBSET %>% 
+  filter(Analyte == "FCoXX_M2_Mek") %>% 
+  filter(Treatment != 12) %>% 
+  filter((Cell_line =="XX" & (Treatment == 0|Treatment == 1000)) | (Cell_line =="XO" & (Treatment == 4|Treatment == 1000)))
+
+
+
+g <- ggplot(Fig7H_pMek, aes(x=x_axis_factor,y=Signal)) +
+  geom_point(aes(x=x_axis_factor,y=Signal,color=Cell_line))+
+  scale_fill_manual(values = MyCellLineColours) +
+  scale_color_manual(values = MyCellLineColours) +
+  stat_summary(fun = mean,geom = "crossbar", size = 0.1, position = position_dodge(width = 1))+
+  ylim(0,max(Fig7H_pMek$Signal)+0.5)+
+  labs(x = "\n MEKi(nM)", 
        y = " Rel. phos. (norm) ",
        title = "pMEK",
        color = "Cell line" )+
@@ -132,25 +212,25 @@ g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek, aes(x=x_axis_factor,
 
 gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
 grid.arrange(gt)
-ggsave("Fig7H_PDDR_XX_XO_compared_pMek_FC_NEW.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
+ggsave("Fig7H_Mek.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
 
-# Test for significance of results : Fig7H
-
-Fig7H_XX = PDDR_All_MekTPSplot_FCoXX_subset_newFCPlot_Mek 
 
 Fig7H_ttest = list()
 the_cell_lines = c("XX","XO")
 for(i in 1:length(the_cell_lines)){
+  #i =2
   
-  data = Fig7H_XX %>% 
+  data = Fig7H_pMek %>% 
     filter(Cell_line == the_cell_lines[i])
   
-  treated_signal = data %>% filter(Treatment == 1000) %>% select(Signal)
-  base_signal = data %>% filter(Treatment != 1000) %>% select(Signal)
+  treated = data %>% filter(Treatment == 1000) 
+  base = data %>% filter(Treatment != 1000) 
   
-  test_result = t.test(base_signal,treated_signal, paired = F)
+  test_result = t.test(base$Signal,treated$Signal, paired = F)
   
-  Fig7H_ttest$cell_line[i] = the_cell_lines[i]
+  Fig7H_ttest$Cell_line[i] = the_cell_lines[i]
+  Fig7H_ttest$base_conc[i] = unique(base$Treatment)
+  Fig7H_ttest$treated_conc[i] = unique(treated$Treatment)
   Fig7H_ttest$base_mean[i] = test_result$estimate[1]
   Fig7H_ttest$treated_mean[i] = test_result$estimate[2]
   Fig7H_ttest$p_value[i] = test_result$p.value
@@ -162,88 +242,32 @@ Fig7H_ttest$p_value = as.numeric(as.character(Fig7H_ttest$p_value))
 Fig7H_ttest <- Fig7H_ttest %>% 
   mutate(sig = ifelse(p_value<0.05,"*",""))
 
-WriteXLS::WriteXLS(Fig7H_ttest, "./OUTPUT_PAPER/Fig7H_Ttest.xls")
+Fig7H_ttest <- Fig7H_ttest %>% 
+  pivot_longer(cols = c("base_conc", "treated_conc"), names_to = "Treatment_conc", values_to = "Treatment") %>% 
+  mutate(Treatment = as.numeric(as.character(Treatment))) %>% 
+  mutate(base_mean = as.numeric(as.character(base_mean))) %>% 
+  mutate(treated_mean = as.numeric(as.character(treated_mean))) %>% 
+  mutate(p_value = as.numeric(as.character(p_value))) %>% 
+  select(-c(Treatment_conc))
 
 
+Fig7H_Mek_Replicates = Fig7H_pMek %>% 
+  select(Cell_line,Replicate,Treatment,log2Treatment,Analyte,Signal) %>% 
+  mutate(Treatment=as.numeric(as.character(Treatment))) %>%
+  pivot_wider(names_from = Replicate,values_from=Signal) %>% 
+  rowwise() %>% 
+  mutate(Mean=mean(c(R1,R2,R3), na.rm = TRUE))
 
-############# Fig7Gii : RAF 
+Fig7H_Mek_data = left_join(Fig7H_Mek_Replicates,Fig7H_ttest, by=c("Treatment","Cell_line")) %>% 
+  arrange(desc(Cell_line)) %>% 
+  mutate(Analyte= gsub("FCoXX_M2_","",Analyte)) %>%
+  mutate(base_mean = ifelse(Treatment != 1000, NA, base_mean)) %>% 
+  mutate(treated_mean = ifelse(Treatment != 1000, NA, treated_mean)) %>% 
+  mutate(p_value = ifelse(Treatment != 1000, NA, p_value)) %>% 
+  mutate(sig = ifelse(Treatment != 1000, NA, sig)) %>% 
+  arrange(Cell_line) %>% 
+  rowwise() %>% 
+  mutate(FoldChange=treated_mean/base_mean)
 
-PDDR_All_MekTPSplot_FCoXX_subset_Raf <- PDDR_All_MekTPSplot_FCoXX %>% 
-  filter((Cell_line == "XX" & Treatment_Fctr == 0)|(Cell_line == "XO" & (Treatment_Fctr == 0|Treatment_Fctr == 4|Treatment_Fctr == 12))) %>%
-  # filter(Cell_line == "XO" & (Treatment == 0|Treatment == 4|Treatment == 12)) %>% 
-  filter(grepl("Raf", Analyte))
+WriteXLS::WriteXLS(Fig7H_Mek_data, "./OUTPUT_PAPER/Fig7H_Mek_data.xls")
 
-#PDDR_All_MekTPSplot_FCoXX_subset_Raf$Treatment_Fctr = factor(PDDR_All_MekTPSplot_FCoXX_subset_Raf$Treatment_Fctr,levels = c("0","4","12"))
-PDDR_All_MekTPSplot_FCoXX_subset_Raf$Treatment_Fctr = factor(PDDR_All_MekTPSplot_FCoXX_subset_Raf$Treatment_Fctr,levels = c(0,4,12))
-
-PDDR_All_MekTPSplot_FCoXX_subset_Raf$x_axis_factor <- paste0(PDDR_All_MekTPSplot_FCoXX_subset_Raf$Cell_line,":",PDDR_All_MekTPSplot_FCoXX_subset_Raf$Treatment_Fctr)
-PDDR_All_MekTPSplot_FCoXX_subset_Raf$x_axis_factor <- factor(PDDR_All_MekTPSplot_FCoXX_subset_Raf$x_axis_factor, 
-                                                             levels = c("XX:0",
-                                                                        "XO:0",
-                                                                        "XO:4",
-                                                                        "XO:12"),
-                                                             labels = c("XX:0",
-                                                                        "0",
-                                                                        "4",
-                                                                        "12"))
-
-
-g <- ggplot(PDDR_All_MekTPSplot_FCoXX_subset_Raf, aes(x=x_axis_factor,y=Signal)) +
-  #geom_point(alpha = 0.8,aes(x=x_axis_factor,y=Signal,color=Cell_line))+
-  geom_point(aes(x=x_axis_factor,y=Signal,color=Cell_line))+
-  scale_fill_manual(values = MyCellLineColours) +
-  scale_color_manual(values = MyCellLineColours) +
-  #facet_grid(~Cell_line, space = "free",scales = "free", switch = "x")+
-  stat_summary(fun = mean,geom = "crossbar", size = 0.1, position = position_dodge(width = 1))+
-  ylim(0,max(PDDR_All_MekTPSplot_FCoXX_subset_Raf$Signal+0.2))+
-  labs(x = "\n MEKi(nM)", #\u03bc is the unicode charachter fro greek
-       y = " Rel. phosp. (norm.)  ",
-       title = "cRAF1",
-       color = "Cell line" )+
-  MyPaperTheme+
-  theme(strip.placement = "outside")
-
-gt=set_panel_size(g,width=unit(2.8,'cm'),height=unit(2.8,'cm'))
-grid.arrange(gt)
-ggsave("Fig7Gii_PDDR_XX_XO_compared_pRaf.pdf", gt, dpi=300, useDingbats=FALSE, path = "./OUTPUT_PAPER") 
-
-# Test for significance of results : Fig7G_Raf
-
-Raf_XX_0= PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
-  filter(Cell_line=="XX" & Treatment_Fctr ==0) 
-
-Raf_XO_0 = PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
-  filter(Cell_line=="XO" & Treatment_Fctr == 0) 
-
-Raf_XO_4 = PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
-  filter(Cell_line=="XO" & Treatment_Fctr == 4)
-
-Raf_XO_12 = PDDR_All_MekTPSplot_FCoXX_subset_Raf %>% 
-  filter(Cell_line=="XO" & Treatment_Fctr == 12) 
-
-
-Fig7G_Raf_compared_groups=list("Raf_XO_0"=Raf_XO_0,"Raf_XO_4"=Raf_XO_4,"Raf_XO_12"=Raf_XO_12)
-Fig7G_Raf_compared_groups_ttest=list()
-
-for(i in 1:length(Fig7G_Raf_compared_groups)) {
-  
-  Fig7G_Raf_compared_groups_ttest$compared_group[i] = names(Fig7G_Raf_compared_groups[i])
-  
-  unpaired_Ttest = t.test(Raf_XX_0$Signal,Fig7G_Raf_compared_groups[[i]]$Signal, paired = FALSE)
-  
-  Fig7G_Raf_compared_groups_ttest$XX_untreated_mean[i] = unpaired_Ttest$estimate[1]
-  Fig7G_Raf_compared_groups_ttest$XO_treated_mean[i] = unpaired_Ttest$estimate[2]
-  Fig7G_Raf_compared_groups_ttest$p_value[i] = unpaired_Ttest$p.value
-  
-}
-
-Fig7G_Raf_compared_groups_ttest <- data.frame((sapply(Fig7G_Raf_compared_groups_ttest,c)))
-Fig7G_Raf_compared_groups_ttest$p_value = as.numeric(as.character(Fig7G_Raf_compared_groups_ttest$p_value))
-Fig7G_Raf_compared_groups_ttest <- Fig7G_Raf_compared_groups_ttest %>% 
-  mutate(sig = ifelse(p_value<0.05,"*",""))
-
-WriteXLS::WriteXLS(Fig7G_Raf_compared_groups_ttest, "./OUTPUT_PAPER/Fig7Gii_Raf_Ttest.xls")
-
-
-
-print("Script 3 executed : Scatter plots saved")
